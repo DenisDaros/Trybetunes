7f14c0c87421f34dd0b10d906cba325e0b48054f
Denis <denisv.daros@gmail.com>
2022-07-02 10:32:42 -0300

    correção lint

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Album from './pages/Album';
 import Favorites from './pages/Favorites';
 import Login from './pages/Login';
