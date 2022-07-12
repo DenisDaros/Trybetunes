bcff1ccb3d654df7689ec8d1def0c9a5bb627cf8
Denis <denisv.daros@gmail.com>
2022-07-12 02:55:22 -0300

    requisito 8 concluido

--- src/components/MusicCard.jsx
+++ src/components/MusicCard.jsx
@@ -1,24 +1,60 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Loading from '../pages/Loading';
 
 class MusicCard extends React.Component {
-  render() {
-    const { albumId } = this.props;
-    return (
-      <>
-        <li>{ albumId.trackName }</li>
-        <audio data-testid="audio-component" src={ albumId.previewUrl } controls>
-          <track kind="captions" />
-          O seu navegador não suporta o elemento
-          {' '}
-          {' '}
-          <code>audio</code>
-          .
-        </audio>
-
-      </>
-    );
+  constructor() {
+    super();
+    this.state = {
+      favorita: false,
+      carregando: false,
+    };
   }
+
+changeCheckBox = async ({ target }) => {
+  const { albumId } = this.props;
+  const { checked } = target;
+  this.setState({ favorita: checked, carregando: true });
+  if (checked) {
+    await addSong(albumId);
+    this.setState({ carregando: false });
+}
+}
+
+render() {
+  const { albumId } = this.props;
+  const { favorita, carregando } = this.state;
+  return (
+    <>
+      {carregando && <Loading />}
+      {!carregando && (
+        <div>
+          <li>{ albumId.trackName }</li>
+          <audio data-testid="audio-component" src={ albumId.previewUrl } controls>
+            <track kind="captions" />
+            O seu navegador não suporta o elemento
+            {' '}
+            {' '}
+            <code>audio</code>
+            .
+          </audio>
+          <label htmlFor="favorita">
+            Favorita
+            <input
+              type="checkbox"
+              checked={ favorita }
+              name="favorita"
+              onChange={ this.changeCheckBox }
+              id="favorita"
+              data-testid={ `checkbox-music-${albumId.trackId}`}
+            />
+          </label>
+        </div>
+      )}
+    </>
+  );
+}
 }
 
 MusicCard.propTypes = {
